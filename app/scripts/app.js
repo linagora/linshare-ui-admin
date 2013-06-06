c2a7dab52caeff8edc0d0341ada46dbcd5d9e1c6
Camille Tjhoa <ctjhoa@linagora.com>
2013-06-06 12:17:03 +0200

    Fresh install of yeoman angularjs minsafe with modules needed for linshare poc

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('linshareFrontendAdminApp', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
