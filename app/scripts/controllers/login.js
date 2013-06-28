37098b479da3797bc8064a649fa38de696f491e0
Camille Tjhoa <ctjhoa@linagora.com>
2013-06-28 11:22:16 +0200

    Add some comment

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,17 +3,22 @@
 app.controller('LoginCtrl', ['$scope', 'Restangular', 'authService', 'loggerService',
   function($scope, Restangular, authService, Logger) {
     $scope.submit = function() {
-      Restangular.all('admin').customGET('authorized', {
-        ignoreAuthModule: true
-      }, {
-        Authorization: 'Basic ' + Base64.encode($scope.login + ':' + $scope.password)
-      }).then(function(data, status, headers, config) {
+      var successCallback = function(data, status, headers, config) {
         Logger.debug("We are logged");
         authService.loginConfirmed();
-      }, function(data, status, headers, config) {
+      };
+      var errorCallback = function(data, status, headers, config) {
         $scope.errorLogin = "Bad credentials";
         Logger.debug("We are not logged");
-      });
+      };
+    
+      Restangular.all('admin').customGET('authorized', {
+        // QueryParams - Bypass the module authService
+        ignoreAuthModule: true
+      }, {
+        // Headers - Add login password
+        Authorization: 'Basic ' + Base64.encode($scope.login + ':' + $scope.password)
+      }).then(successCallback, errorCallback);
     }
   }
 ]);
