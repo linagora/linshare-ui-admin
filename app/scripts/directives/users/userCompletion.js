c2314a0ca7f43fb641fb626c4f24492b00ad03d6
Camille Tjhoa <ctjhoa@linagora.com>
2013-08-26 17:21:17 +0200

    Fix user completion

--- app/scripts/directives/users/userCompletion.js
+++ app/scripts/directives/users/userCompletion.js
@@ -15,7 +15,16 @@ app.directive('lsUserCompletion', [
       controller: ['$scope', 'Restangular', 'loggerService',
         function($scope, Restangular, Logger) {
           $scope.users = function(pattern) {
-            return Restangular.all('users').all('search').all(pattern).getList();
+            return Restangular.all('users').one('search', pattern).get().then(function success(users) {
+              var restangularCollection = [];
+              angular.forEach(users, function(user, key) {
+                restangularCollection.push(Restangular.restangularizeElement(null, user, 'users'));
+              });
+              return restangularCollection;
+            }, function error() {
+              Logger.error('Unable to reach completion service');
+            });
+
           }
         }
       ],
