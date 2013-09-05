adda082a3de3cf04d32b92cc7e22dd40222494f2
Camille Tjhoa <ctjhoa@linagora.com>
2013-09-05 10:47:52 +0200

    Clean domain create form

--- app/scripts/directives/domains/domainCreateForm.js
+++ app/scripts/directives/domains/domainCreateForm.js
@@ -6,7 +6,11 @@ app.directive('lsDomainCreateForm', [
       restrict: 'A',
       transclude: false,
       // Use new scope to not interfere with edit domain form
-      scope: {},
+      scope: {
+        domainPolicies: '=',
+        userRoles: '=',
+        locales: '='
+      },
       link: function(scope, element, attrs) {
         scope.disableProvider = false;
         scope.showCreationModal = false;
@@ -44,32 +48,6 @@ app.directive('lsDomainCreateForm', [
               notificationService.addSuccess('P_Domains-DomainPatterns_CreateSuccess');
             });
           };
-
-          // Has it's a new scope you need to reload all possible choices
-
-          $scope.ldapConnections = [];
-          manageDomainService.getAllLdapConnections(function success(ldapConnections) {
-            angular.forEach(ldapConnections, function(ldapConnection, key) {
-              $scope.ldapConnections.push(ldapConnection.identifier);
-            });
-          });
-          $scope.domainPatterns = [];
-          manageDomainService.getAllDomainPatterns(function success(domainPatterns) {
-            angular.forEach(domainPatterns, function(domainPattern, key) {
-              $scope.domainPatterns.push(domainPattern.identifier);
-            });
-          });
-          $scope.domainPolicies = [];
-          manageDomainService.getAllDomainPolicies(function success(domainPolicies) {
-            angular.forEach(domainPolicies, function(domainPolicy, key) {
-              $scope.domainPolicies.push(domainPolicy.identifier);
-            });
-          });
-          $scope.userRoles = [];
-          manageDomainService.getAllUserRoles(function success(userRoles) {
-            $scope.userRoles = userRoles;
-          });
-          $scope.locales = manageDomainService.getAllLocales;
         }
       ],
       templateUrl: '/views/templates/domains/domain_create_form.html',
