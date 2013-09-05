237fcc95b31eb591dc0cc126a1288b1d31717c28
Camille Tjhoa <ctjhoa@linagora.com>
2013-09-05 10:30:25 +0200

    Remove unused code in userLogged service

--- app/scripts/services/userLogged.js
+++ app/scripts/services/userLogged.js
@@ -5,14 +5,8 @@
 angular.module('myApp.services')
   .factory('userLoggedService', ['$rootScope', 'Restangular', 'loggerService',
   function($rootScope, Restangular ,Logger) {
-    var UserLogged = {};
     Restangular.all('authentication').customGET('authorized').then(function success(user) {
       $rootScope.userLogged = user;
     });
-    return {
-      get: function() {
-        return UserLogged;
-      }
-    }
   }
 ]);
