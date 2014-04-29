c44a45f0739ab04be6b47d35f13be8e94ae6393d
Camille Tjhoa <ctjhoa@linagora.com>
2014-04-29 16:24:59 +0200

    Add password strengthscore

--- app/scripts/controllers/password.js
+++ app/scripts/controllers/password.js
@@ -6,8 +6,8 @@ angular.module('linshareAdminApp')
       function ($scope, $location, $log, Authentication) {
         function Rule(description, validator) {
           this.description = description;
-          this.className = function () {
-            return validator() ? "icon-check" : "icon-exclamation-sign";
+          this.validator = function() {
+            return validator();
           }
         }
         $scope.newPwdRetyped = '';
@@ -16,20 +16,38 @@ angular.module('linshareAdminApp')
           'newPwd' : ''
         };
         $scope.rules = [
+          new Rule('Contain a symbol', function () {
+            var pwd = $scope.password.newPwd;
+            return pwd && /[$-/:-?{-~!"^_`\[\]]/g.test(pwd);
+          }),
+          new Rule('Length longer than 8 char', function () {
+            var pwd = $scope.password.newPwd;
+            return pwd && pwd.length >= 8;
+          }),
           new Rule('Contain a digit', function () {
-            return $scope.password.newPwd.match(/[0-9]/);
+            var pwd = $scope.password.newPwd;
+            return pwd && /[0-9]+/.test(pwd);
           }),
           new Rule('Contain an upper case character', function () {
-            return $scope.password.newPwd.match(/[A-Z]/);
+            var pwd = $scope.password.newPwd;
+            return pwd && /[A-Z]+/.test(pwd);
           }),
           new Rule('Contain a lower case character', function () {
-            return $scope.password.newPwd.match(/[a-z]/);
+            var pwd = $scope.password.newPwd;
+            return pwd && /[a-z]+/.test(pwd);
           })
         ];
         $scope.match = function() {
           return !$scope.password.newPwd ||
                  !$scope.password.newPwd ||
                  $scope.password.newPwd == $scope.newPwdRetyped;
+        };
+        $scope.strengthScore = function() {
+          var strength = 0;
+          angular.forEach($scope.rules, function(r) {
+            strength += r.validator();
+          });
+          return strength * 100 / $scope.rules.length;
         };
         $scope.redirectToHome = function () {
           $location.path('/');
