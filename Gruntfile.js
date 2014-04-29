2c3b20ca41527333eeadc28f6ff0289f4306e30d
Camille Tjhoa <ctjhoa@linagora.com>
2014-04-29 17:11:33 +0200

    prevent hash of config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -173,7 +173,8 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
+            '<%= yeoman.dist %>/styles/fonts/*',
+            '!<%= yeoman.dist %>/scripts/config.js'
           ]
         }
       }
