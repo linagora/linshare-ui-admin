c244e296f80e3f7c6e06ac3b020af5923806d976
Frédéric MARTIN <fmartin@linagora.com>
2014-06-10 15:20:16 +0200

    improve build script.

--- build.sh
+++ build.sh
@@ -6,12 +6,11 @@ l_version=$(grep version package.json | cut -d'"' -f4)
 l_output="linshare-ui-admin-${l_version}.tar.bz2"
 
 echo "INFO: Cleaning ..."
-rm -fr linshare-ui-admin
 rm -fr linshare-ui-admin-*
 echo "INFO: Building ..."
 grunt build
 echo "INFO: Packaging..."
-mv dist linshare-ui-admin
-tar cjf linshare-ui-admin-${l_version}.tar.bz2 linshare-ui-admin
+mv dist linshare-ui-admin-${l_version}
+tar cjf linshare-ui-admin-${l_version}.tar.bz2 linshare-ui-admin-${l_version}
 echo "INFO: Done"
 
