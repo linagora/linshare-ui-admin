ef277d9c2800a5b4751268a72efe1e5fc70ac58a
Frédéric MARTIN <fmartin@linagora.com>
2014-06-12 12:58:52 +0200

    add dev profile to build script

--- build.sh
+++ build.sh
@@ -2,15 +2,25 @@
 set -e
 set -x
 
+l_mode=$1
+
 l_version=$(grep version package.json | cut -d'"' -f4)
 l_output="linshare-ui-admin-${l_version}.tar.bz2"
+l_git_uuid=$(git log -n 1 --format=oneline|cut -d' ' -f1)
+l_dist=linshare-ui-admin-${l_version}
+
 
 echo "INFO: Cleaning ..."
 rm -fr linshare-ui-admin-*
 echo "INFO: Building ..."
 grunt build
 echo "INFO: Packaging..."
-mv dist linshare-ui-admin-${l_version}
-tar cjf linshare-ui-admin-${l_version}.tar.bz2 linshare-ui-admin-${l_version}
+if [ "${l_mode}" == "dev" ] ; then
+  l_dist="linshare-ui-admin-${l_version}-${l_git_uuid}"
+  l_output="linshare-ui-admin-${l_version}-${l_git_uuid}.tar.bz2"
+fi
+mv -v dist ${l_dist}
+tar cjf ${l_output} ${l_dist}
+
 echo "INFO: Done"
 
