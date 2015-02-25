62f07f130f5dd83d5fde9e7f8658448d19dadbcd
Frédéric MARTIN <fmartin@linagora.com>
2015-02-25 11:34:52 +0100

    Adding more target to build.sh (minification or not)

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
-set -x
+#set -x
 
 l_mode=$1
+
+
+function usage()
+{
+   echo "Choose your mode : dev dev-nomin, prod or prod-nomin"
+   exit 0
+}
+
+if [ "${l_mode}" == "" ] ; then
+  usage
+fi
+
 
 l_version=$(grep version package.json | cut -d'"' -f4)
 l_output="linshare-ui-admin-${l_version}.tar.bz2"
@@ -13,19 +25,34 @@ l_dist=linshare-ui-admin-${l_version}
 echo "INFO: Cleaning ..."
 rm -fr linshare-ui-admin-*
 echo "INFO: Building ..."
-./node_modules/.bin/grunt build
+if [ "${l_mode}" == "dev" ] || [ "${l_mode}" == "prod" ] ; then
+  ./node_modules/.bin/grunt build
+elif [ "${l_mode}" == "dev-nomin" ] || [ "${l_mode}" == "prod-nomin" ] ; then
+  ./node_modules/.bin/grunt build --gruntfile noMinifyerGruntfile.js
+else
+  usage
+fi
+
 echo "INFO: Packaging..."
 if [ "${l_mode}" == "dev" ] ; then
   l_dist="linshare-ui-admin-${l_version}-${l_git_uuid}"
   l_output="linshare-ui-admin-${l_version}-${l_git_uuid}.tar.bz2"
+elif [ "${l_mode}" == "dev-nomin" ] ; then
+  l_dist="linshare-ui-admin-${l_version}-nomin-${l_git_uuid}"
+  l_output="linshare-ui-admin-${l_version}-nomin-${l_git_uuid}.tar.bz2"
+elif [ "${l_mode}" == "prod-nomin" ] ; then
+  l_dist="linshare-ui-admin-${l_version}-nomin"
+  l_output="linshare-ui-admin-${l_version}-nomin.tar.bz2"
 fi
 
 mv -v dist ${l_dist}
-if [ "${l_mode}" != "dev" ] ; then
+if [ "${l_mode}" == "dev" ] || [ "${l_mode}" == "dev-nomin" ] ; then
   sed -i -e 's/debug: true/debug: false/g' ${l_dist}/scripts/config.js
 fi
 
 tar cjf ${l_output} ${l_dist}
+mkdir -p distrib
+mv -v ${l_output} distrib/
 
 echo "INFO: Done"
 
