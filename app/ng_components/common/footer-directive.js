d8d34f29b3e5f535c86ad22c5c5ce1caa4c3270b
Anaïs Siba <asiba@linagora.com>
2015-07-02 15:35:45 +0200

    Bug #962 - correction text and translated footer

--- app/ng_components/common/footer-directive.js
+++ app/ng_components/common/footer-directive.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('linshareAdminApp').directive('lsFooter',
-  ['lsAppConfig',
-	function(lsAppConfig) {
+	function(lsAppConfig, $rootScope, $translate, $compile, $http, $templateCache) {
+		var getTemplate = function () {
+      if (lsAppConfig.license) {
+        return 'ng_components/common/footer_' + $translate.use() + '.tpl.html';
+      }
+	  };
+    var linker = function (scope, element) {
+			scope.template = getTemplate();
+			$rootScope.$on('$translateChangeSuccess', function () {
+        scope.template = getTemplate();
+      });
+		};
 		return {
 			restrict: 'A',
 			transclude: true,
 			scope: false,
+			link: linker,
 			templateUrl: (lsAppConfig.license) ? 'ng_components/common/footer.tpl.html' : '',
 			replace: false
 		};
-	}
-	]);
+});
