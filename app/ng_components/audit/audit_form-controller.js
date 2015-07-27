6fdd79c5715e3e63cc82e46276ed7941b8c922ee
Anaïs Siba <asiba@linagora.com>
2015-07-27 18:47:32 +0200

    Bug #1001 - Translated date and set starting date to 7 days ago and end date to now

--- app/ng_components/audit/audit_form-controller.js
+++ app/ng_components/audit/audit_form-controller.js
@@ -2,8 +2,8 @@
 
 angular.module('linshareAdminApp')
   .controller('AuditFormCtrl',
-    ['$scope', '$filter', '$log', 'ngTableParams', 'Audit', 'selectOptions', 'authenticatedUser',
-    function($scope, $filter, $log, ngTableParams, Audit, selectOptions, authenticatedUser) {
+    ['$rootScope', '$scope', '$filter', '$log', '$translate', '$locale', 'ngTableParams', 'Audit', 'selectOptions', 'authenticatedUser',
+    function($rootScope, $scope, $filter, $log, $translate, $locale, ngTableParams, Audit, selectOptions, authenticatedUser) {
       $scope.criteria = {};
       $scope.actorMails = '';
       $scope.targetMails = '';
@@ -14,6 +14,225 @@ angular.module('linshareAdminApp')
         from: false,
         to : false
       };
+      // This solution is used to translate the angular ui bootstrap
+      // Issue : http://stackoverflow.com/questions/19094415/how-to-translate-angular-ui-bootstrap-datepicker
+      // In the futur we will need to find a better solution
+      // to Parse, validate, manipulate, and display dates in JavaScript in some language
+      var locales = {
+        fr: {
+          "DATETIME_FORMATS": {
+            "AMPMS": [
+              "AM",
+              "PM"
+            ],
+            "DAY": [
+              "dimanche",
+              "lundi",
+              "mardi",
+              "mercredi",
+              "jeudi",
+              "vendredi",
+              "samedi"
+            ],
+            "MONTH": [
+              "janvier",
+              "f\u00e9vrier",
+              "mars",
+              "avril",
+              "mai",
+              "juin",
+              "juillet",
+              "ao\u00fbt",
+              "septembre",
+              "octobre",
+              "novembre",
+              "d\u00e9cembre"
+            ],
+            "SHORTDAY": [
+              "dim.",
+              "lun.",
+              "mar.",
+              "mer.",
+              "jeu.",
+              "ven.",
+              "sam."
+            ],
+            "SHORTMONTH": [
+              "janv.",
+              "f\u00e9vr.",
+              "mars",
+              "avr.",
+              "mai",
+              "juin",
+              "juil.",
+              "ao\u00fbt",
+              "sept.",
+              "oct.",
+              "nov.",
+              "d\u00e9c."
+            ],
+            "fullDate": "EEEE d MMMM y",
+            "longDate": "d MMMM y",
+            "medium": "d MMM y HH:mm:ss",
+            "mediumDate": "d MMM y",
+            "mediumTime": "HH:mm:ss",
+            "short": "dd/MM/yy HH:mm",
+            "shortDate": "dd/MM/yy",
+            "shortTime": "HH:mm"
+          },
+          "NUMBER_FORMATS": {
+            "CURRENCY_SYM": "\u20ac",
+            "DECIMAL_SEP": ",",
+            "GROUP_SEP": "\u00a0",
+            "PATTERNS": [
+              {
+                "gSize": 3,
+                "lgSize": 3,
+                "macFrac": 0,
+                "maxFrac": 3,
+                "minFrac": 0,
+                "minInt": 1,
+                "negPre": "-",
+                "negSuf": "",
+                "posPre": "",
+                "posSuf": ""
+              },
+              {
+                "gSize": 3,
+                "lgSize": 3,
+                "macFrac": 0,
+                "maxFrac": 2,
+                "minFrac": 2,
+                "minInt": 1,
+                "negPre": "(",
+                "negSuf": "\u00a0\u00a4)",
+                "posPre": "",
+                "posSuf": "\u00a0\u00a4"
+              }
+            ]
+          },
+          "id": "fr-fr",
+          "pluralCat": function (n) {
+            if (n >= 0 && n <= 2 && n != 2) {
+              return PLURAL_CATEGORY.ONE;
+            }
+            return PLURAL_CATEGORY.OTHER;
+          }
+        },
+        en: {
+          "DATETIME_FORMATS": {
+            "AMPMS": [
+              "AM",
+              "PM"
+            ],
+            "DAY": [
+              "Sunday",
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+              "Saturday"
+            ],
+            "MONTH": [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December"
+            ],
+            "SHORTDAY": [
+              "Sun",
+              "Mon",
+              "Tue",
+              "Wed",
+              "Thu",
+              "Fri",
+              "Sat"
+            ],
+            "SHORTMONTH": [
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec"
+            ],
+            "fullDate": "EEEE, MMMM d, y",
+            "longDate": "MMMM d, y",
+            "medium": "MMM d, y h:mm:ss a",
+            "mediumDate": "MMM d, y",
+            "mediumTime": "h:mm:ss a",
+            "short": "M/d/yy h:mm a",
+            "shortDate": "M/d/yy",
+            "shortTime": "h:mm a"
+          },
+          "NUMBER_FORMATS": {
+            "CURRENCY_SYM": "$",
+            "DECIMAL_SEP": ".",
+            "GROUP_SEP": ",",
+            "PATTERNS": [
+              {
+                "gSize": 3,
+                "lgSize": 3,
+                "macFrac": 0,
+                "maxFrac": 3,
+                "minFrac": 0,
+                "minInt": 1,
+                "negPre": "-",
+                "negSuf": "",
+                "posPre": "",
+                "posSuf": ""
+              },
+              {
+                "gSize": 3,
+                "lgSize": 3,
+                "macFrac": 0,
+                "maxFrac": 2,
+                "minFrac": 2,
+                "minInt": 1,
+                "negPre": "(\u00a4",
+                "negSuf": ")",
+                "posPre": "\u00a4",
+                "posSuf": ""
+              }
+            ]
+          },
+          "id": "en-us",
+          "pluralCat": function (n) {
+            if (n == 1) {
+              return PLURAL_CATEGORY.ONE;
+            }
+            return PLURAL_CATEGORY.OTHER;
+          }
+        }
+      };
+
+      // initializes $locale with french locale
+      angular.copy(locales[$translate.use()], $locale);
+      $scope.criteria.beforeDate = new Date(new Date() - 7 * 24 * 60 * 60 * 1000);
+      $scope.criteria.afterDate = new Date();
+
+      $rootScope.$on('$translateChangeSuccess', function () {
+        // changes dt to apply the $locale changes
+        angular.copy(locales[$translate.use()], $locale);
+
+        $scope.criteria.beforeDate = new Date(new Date() - 7 * 24 * 60 * 60 * 1000);
+        $scope.criteria.afterDate = new Date();
+      });
 
       $scope.open = function(key, $event) {
         $event.preventDefault();
--- app/ng_components/common/boxbody-directive.js
+++ app/ng_components/common/boxbody-directive.js
@@ -1,31 +1,13 @@
 'use strict';
 
-angular.module('linshareAdminApp').directive('boxBody', ['$window', '$state', '$rootScope',
-  function($window, $state, $rootScope) {
-    var getTreeDomain = function() {
-      return ($state.current.views && $state.current.views.tree) ? true : false;
-    };
-    var setLess = function(treeDomain) {
-      return (treeDomain) ? 330 : 360;
-    };
-    var setBodyHeight = function(element, less) {
-      var bodyHeight = ($window.innerHeight - less);
-      element.css('height', ( bodyHeight > 400 ) ? bodyHeight : 400);
-    };
+angular.module('linshareAdminApp').directive('boxTreeDomain', ['$window',
+  function($window) {
     return {
       restrict: 'C',
-      scope: false,
       link: function(scope, element, attrs) {
-        var less = setLess(getTreeDomain());
-
-        $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams){
-          less = setLess(getTreeDomain());
-          setBodyHeight(element, less);
-        });
-
-        setBodyHeight(element, less);
-      },
-      replace: false
+        var bodyHeight = ($window.innerHeight - 250);
+        element.css('height', ( bodyHeight > 400 ) ? bodyHeight : 400);
+      }
     };
   }
 ]);
