72fe8e0639a201cfa887b40d0081220ba63fd806
Ismail ANWAR-BAIG <ianwar@linagora.com>
2017-07-24 17:46:22 +0200

    Improved audit details dropdown

--- app/ng_components/auditv2/directives/audit-details/audit-details.directive.js
+++ app/ng_components/auditv2/directives/audit-details/audit-details.directive.js
@@ -11,7 +11,7 @@
 
   /**
    * @namespace auditDetails
-   * @desc Popover with details of one audit action
+   * @desc Dropdown with details of one audit action
    * @example <div audit-details="{{auditActionObject}}"></div>
    * @memberOf linshareAdminApp
    */
@@ -33,7 +33,7 @@
      * @memberOf linshareAdminApp.auditDetails
      */
     function linkFn(scope, element) {
-      var dropdownList = angular.element(element[0].querySelector('.ls-dropdown'));
+      var dropdownList = angular.element(element[0].querySelector('.audit-dropdown'));
       scope.$watch(function() {
         return element[0].querySelector('.btn-group').className;
       }, function(className) {
@@ -41,10 +41,10 @@
           attachToBody(dropdownList, element);
         }
         if (className.indexOf('open') !== -1) {
-          dropdownList.addClass('ls-dropdown-open');
+          dropdownList.addClass('audit-dropdown-open');
           dropdownList.removeClass('hide');
         } else {
-          dropdownList.removeClass('ls-dropdown-open');
+          dropdownList.removeClass('audit-dropdown-open');
           dropdownList.addClass('hide');
         }
       });
@@ -56,14 +56,7 @@
           angular.element('body').append(dropdownList);
           dropdownList.css({
             top: element.offset().top + 'px',
-            left: element.offset().left + 'px',
-            width: '300px',
-            paddingLeft: '10px',
-            paddingRight: '10px',
-            paddingBottom: '15px',
-            marginLeft: '-250px',
-            marginTop: '30px',
-            display: 'block'
+            left: element.offset().left + 'px'
           });
         }
       }
