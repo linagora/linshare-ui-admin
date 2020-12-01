e1d1d9373ef60096604197430dc3ae81a6725edc
Dung Trung PHAM <dtpham@linagora.com>
2020-12-01 10:07:00 +0700

    Issue #106 Remove the old app

--- app/ng_components/auditv2/constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  /* jshint undef: true */
-  /* global _:false */
-  angular
-    .module('linshareAdminApp')
-    .constant('_', _);
-})();
--- app/ng_components/dashboard/dashboard-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('linshareAdminApp')
-  .controller('DashbordCtrl',
-     ['$scope', '$filter', '$log', 'authenticatedUser',
-      function($scope, $filter, $log, authenticatedUser) {
-        $scope.userDomain = authenticatedUser.domain;
-      }
-    ]);
