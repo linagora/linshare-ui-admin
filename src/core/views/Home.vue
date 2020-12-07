7f75cb0245b6def5523aa312a37d0ad80a8e0d9a
Dung Trung PHAM <dtpham@linagora.com>
2020-12-07 15:54:26 +0700

    Issue #107 Remove generated vue components

--- src/core/views/Home.vue
+++ src/core/views/Home.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <a-button type="primary">
-      Primary
-    </a-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/core/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-})
+  name: 'Home'
+});
 </script>
