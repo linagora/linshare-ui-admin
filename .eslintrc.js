9192a557e5126fea95aea6454b76b02becb070fc
Dung Trung PHAM <dtpham@linagora.com>
2020-12-14 11:23:03 +0700

    Add semicolon eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'semi-style': ['error', 'last'],
-    'semi': ['error', 'always']
+    'semi': 0,
+    '@typescript-eslint/semi': ['error', 'always']
   },
   overrides: [
     {
