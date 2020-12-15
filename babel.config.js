a1107212f79f822c0cc8ae74190e1922fdd1a0ab
Dung Trung PHAM <dtpham@linagora.com>
2020-12-15 11:52:09 +0700

    Issue #106: Add babel import plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'ant-design-vue',
+        libraryDirectory: 'es'
+      }
+    ]
   ]
 };
