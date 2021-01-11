7316d4c51689ce9e29fd3ad037935cb5c5c31a5a
Dung Trung PHAM <dtpham@linagora.com>
2021-01-11 15:43:55 +0700

    Issue #113: Use Admin API v5

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API: {
-    BASE_URL: 'linshare/webservice/rest/admin/v4',
+    BASE_URL: 'linshare/webservice/rest/admin/v5',
     DEFAULT_HEADERS: {
       'WWW-No-Authenticate': 'linshare'
     }
