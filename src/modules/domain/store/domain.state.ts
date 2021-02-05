425f76f725aede72e345cba67bfbbbd960aba8ea
Bao Ngoc PHAM <bnpham@linagora.com>
2021-02-05 17:31:51 +0700

    Issue #117: Refactor domain store

--- src/modules/domain/store/domain.state.ts
+++ src/modules/domain/store/domain.state.ts
@@ -3,3 +3,9 @@ import Domain from '@/modules/domain/type/Domain';
 export interface DomainState {
   domains: Domain[];
 }
+
+const state: DomainState = {
+  domains: []
+};
+
+export default state;
--- src/modules/domain/store/index.ts
+++ src/modules/domain/store/index.ts
@@ -1,11 +1,7 @@
-import { DomainState } from './domain.state';
+import state from './domain.state';
 import getters from './domain.getters';
 import actions from './domain.actions';
 import mutations from './domain.mutations';
-
-const state: DomainState = {
-  domains: []
-};
 
 export default {
   namespaced: true,
