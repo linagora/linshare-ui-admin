37b747696a1003147d8a36dd33d2b8bc22a76b87
Dung Trung PHAM <dtpham@linagora.com>
2021-08-03 18:31:54 +0700

    Issue #152: Use hash routing

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { requiresAuthGuard } from '@/modules/auth/router';
 import routes from './routes';
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 });
 
