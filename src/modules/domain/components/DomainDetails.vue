bbf1077c388ec362713709a53d87e555a1336b55
Dung Trung PHAM <dtpham@linagora.com>
2021-09-01 14:51:48 +0700

    Issue #157: Have a separate component for domain form

--- src/modules/domain/components/DomainDetails.vue
+++ src/modules/domain/components/DomainDetails.vue
@@ -17,67 +17,7 @@
 
   <a-row :gutter="24" v-else>
     <a-col :span="12">
-      <a-form>
-        <a-form-item v-bind="validateInfos.name">
-          <div class="input-container">
-            <label>{{ $t('DOMAIN.FIELDS.NAME') }}</label>
-            <a-input v-model:value="formState.name"></a-input>
-          </div>
-        </a-form-item>
-
-        <a-form-item>
-          <div class="input-container">
-            <label>{{ $t('DOMAIN.FIELDS.DESCRIPTION') }}</label>
-            <a-textarea autosize v-model:value="formState.description"></a-textarea>
-          </div>
-        </a-form-item>
-
-        <a-form-item v-if="!isRootDomain">
-          <div class="input-container">
-            <label>{{ $t('DOMAIN.FIELDS.DEFAULT_USER_ROLE') }}</label>
-            <a-select v-model:value="formState.defaultUserRole">
-              <a-select-option value="SIMPLE">
-                {{ $t('USERS.DETAIL_USER.ROLE_SIMPLE') }}
-              </a-select-option>
-              <a-select-option value="ADMIN">
-                {{ $t('USERS.DETAIL_USER.ROLE_ADMIN') }}
-              </a-select-option>
-              <a-select-option disabled value="SUPERADMIN">
-                {{ $t('USERS.DETAIL_USER.ROLE_SUPERADMIN') }}
-              </a-select-option>
-            </a-select>
-            <small>{{ $t('DOMAIN.FIELDS.DEFAULT_USER_ROLE_HELPER') }}</small>
-          </div>
-        </a-form-item>
-
-        <a-form-item v-if="!isRootDomain">
-          <div class="input-container" >
-            <label>{{ $t('DOMAIN.FIELDS.NOTIFICATION_LANGUAGE') }}</label>
-            <a-select v-model:value="formState.defaultEmailLanguage">
-              <a-select-option value="ENGLISH">
-                {{ $t('LOCALE.ENGLISH') }}
-              </a-select-option>
-              <a-select-option value="FRENCH">
-                {{ $t('LOCALE.FRENCH') }}
-              </a-select-option>
-              <a-select-option value="RUSSIAN">
-                {{ $t('LOCALE.RUSSIAN') }}
-              </a-select-option>
-            </a-select>
-            <small>{{ $t('DOMAIN.FIELDS.NOTIFICATION_LANGUAGE_HELPER') }}</small>
-          </div>
-        </a-form-item>
-
-        <div class="form-actions">
-          <a-button class="reset" @click="reset">
-            {{ $t('GENERAL.RESET') }}
-          </a-button>
-
-          <a-button type='primary' :loading="saving" @click="onSubmit">
-            {{ $t('GENERAL.SAVE') }}
-          </a-button>
-        </div>
-      </a-form>
+      <DomainForm :data="currentDomain"/>
     </a-col>
 
     <a-col :span="10" :offset="2">
@@ -124,95 +64,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, reactive, ref, watchEffect } from 'vue';
+import { defineComponent, computed } from 'vue';
 import { useStore } from 'vuex';
-import { useI18n } from 'vue-i18n';
 import useBreadcrumbs from '@/core/hooks/useBreadcrumbs';
 import PageTitle from '@/core/components/PageTitle.vue';
-import Domain from '@/modules/domain/type/Domain';
+import DomainForm from '@/modules/domain/components/DomainForm.vue';
 import Status from '@/core/types/Status';
-import { Form, message } from 'ant-design-vue';
-
-const useForm = Form.useForm;
 
 export default defineComponent({
   name: 'DomainDetails',
   components: {
-    PageTitle
+    PageTitle,
+    DomainForm
   },
   setup () {
-    const { t, locale } = useI18n();
     const store = useStore();
     const currentDomain = computed(() => store.getters['Domain/getCurrentDomain']);
     const loadingDomain = computed(() => store.getters['Domain/getStatus']('currentDomain') === Status.LOADING);
-    const saving = ref(false);
     const { breadcrumbs } = useBreadcrumbs();
-
-    const formState = reactive< Omit<Domain, 'uuid'> >({
-      name: currentDomain.value.name,
-      description: currentDomain.value.description,
-      defaultUserRole: currentDomain.value.defaultUserRole,
-      defaultEmailLanguage: currentDomain.value.defaultEmailLanguagee
-    });
-    const formRules = reactive({
-      name: [
-        {
-          required: true,
-          message: t('DOMAIN.FIELDS.NAME_REQUIRED', locale.value)
-        },
-        {
-          max: 255,
-          message: t('DOMAIN.FIELDS.NAME_MAX_LENGTH', locale.value)
-        }
-      ]
-    });
-    const { validate, validateInfos } = useForm(formState, formRules);
-
-    watchEffect(() => {
-      if (currentDomain.value.uuid) {
-        formState.name = currentDomain.value.name;
-        formState.description = currentDomain.value.description;
-        formState.defaultEmailLanguage = currentDomain.value.defaultEmailLanguage;
-        formState.defaultUserRole = currentDomain.value.defaultUserRole;
-      }
-    });
-
-    async function onSubmit () {
-      saving.value = true;
-
-      try {
-        await validate();
-      } catch (error) {
-        saving.value = false;
-        return;
-      }
-
-      try {
-        await store.dispatch('Domain/updateDomain', { ...currentDomain.value, ...formState });
-        message.success(t('MESSAGES.UPDATE_SUCCESS'));
-      } catch (error) {
-        message.error(error.message || t('ERRORS.COMMON_MESSAGE'));
-      } finally {
-        saving.value = false;
-      }
-    }
-
-    function reset () {
-      formState.name = currentDomain.value.name;
-      formState.description = currentDomain.value.description;
-      formState.defaultEmailLanguage = currentDomain.value.defaultEmailLanguage;
-      formState.defaultUserRole = currentDomain.value.defaultUserRole;
-    }
 
     return {
       breadcrumbs,
       currentDomain,
-      formState,
       loadingDomain,
-      onSubmit,
-      reset,
-      saving,
-      validateInfos,
       isRootDomain: computed(() => store.getters['Domain/isRootDomain'])
     };
   }
@@ -225,23 +99,6 @@ export default defineComponent({
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-
-  .input-container {
-    label {
-      display: inline-block;
-      font-weight: 600;
-      margin-bottom: 10px;
-    }
-
-    .ant-select {
-      width: 100%;
-    }
-
-    small {
-      font-style: italic;
-      color: @text-color-secondary;
-    }
   }
 
   .info-block-container {
@@ -259,12 +116,6 @@ export default defineComponent({
       .title {
         color: @text-color-secondary;
       }
-    }
-  }
-
-  .form-actions {
-    .reset {
-      margin-right: 10px;
     }
   }
 
---
+++ src/modules/domain/components/DomainForm.vue
@@ -0,0 +1,183 @@
+<template>
+  <a-form>
+    <a-form-item v-bind="validateInfos.name">
+      <div class="input-container">
+        <label>{{ $t("DOMAIN.FIELDS.NAME") }}</label>
+        <a-input v-model:value="formState.name"></a-input>
+      </div>
+    </a-form-item>
+
+    <a-form-item>
+      <div class="input-container">
+        <label>{{ $t("DOMAIN.FIELDS.DESCRIPTION") }}</label>
+        <a-textarea autosize v-model:value="formState.description"></a-textarea>
+      </div>
+    </a-form-item>
+
+    <a-form-item v-if="!isRootDomain">
+      <div class="input-container">
+        <label>{{ $t("DOMAIN.FIELDS.DEFAULT_USER_ROLE") }}</label>
+        <a-select v-model:value="formState.defaultUserRole">
+          <a-select-option value="SIMPLE">
+            {{ $t("USERS.DETAIL_USER.ROLE_SIMPLE") }}
+          </a-select-option>
+          <a-select-option value="ADMIN">
+            {{ $t("USERS.DETAIL_USER.ROLE_ADMIN") }}
+          </a-select-option>
+          <a-select-option disabled value="SUPERADMIN">
+            {{ $t("USERS.DETAIL_USER.ROLE_SUPERADMIN") }}
+          </a-select-option>
+        </a-select>
+        <small>{{ $t("DOMAIN.FIELDS.DEFAULT_USER_ROLE_HELPER") }}</small>
+      </div>
+    </a-form-item>
+
+    <a-form-item v-if="!isRootDomain">
+      <div class="input-container">
+        <label>{{ $t("DOMAIN.FIELDS.NOTIFICATION_LANGUAGE") }}</label>
+        <a-select v-model:value="formState.defaultEmailLanguage">
+          <a-select-option value="ENGLISH">
+            {{ $t("LOCALE.ENGLISH") }}
+          </a-select-option>
+          <a-select-option value="FRENCH">
+            {{ $t("LOCALE.FRENCH") }}
+          </a-select-option>
+          <a-select-option value="RUSSIAN">
+            {{ $t("LOCALE.RUSSIAN") }}
+          </a-select-option>
+        </a-select>
+        <small>{{ $t("DOMAIN.FIELDS.NOTIFICATION_LANGUAGE_HELPER") }}</small>
+      </div>
+    </a-form-item>
+
+    <div class="form-actions">
+      <a-button class="reset" @click="reset">
+        {{ $t("GENERAL.RESET") }}
+      </a-button>
+
+      <a-button type="primary" :loading="saving" @click="onSubmit">
+        {{ $t("GENERAL.SAVE") }}
+      </a-button>
+    </div>
+  </a-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, ref, reactive, watchEffect, PropType } from 'vue';
+import { useStore } from 'vuex';
+import { useI18n } from 'vue-i18n';
+import { Form, message } from 'ant-design-vue';
+
+import Domain from '@/modules/domain/type/Domain';
+
+const useForm = Form.useForm;
+
+export default defineComponent({
+  name: 'DomainDetails',
+  props: {
+    data: {
+      default: () => ({}),
+      type: Object as PropType<Domain>
+    }
+  },
+  setup (props) {
+    const { t, locale } = useI18n();
+    const store = useStore();
+    const saving = ref(false);
+
+    const formState = reactive< Omit<Domain, 'uuid'> >({
+      name: props.data.name,
+      description: props.data.description,
+      defaultUserRole: props.data.defaultUserRole,
+      defaultEmailLanguage: props.data.defaultEmailLanguage
+    });
+    const formRules = reactive({
+      name: [
+        {
+          required: true,
+          message: t('DOMAIN.FIELDS.NAME_REQUIRED', locale.value)
+        },
+        {
+          max: 255,
+          message: t('DOMAIN.FIELDS.NAME_MAX_LENGTH', locale.value)
+        }
+      ]
+    });
+    const { validate, validateInfos } = useForm(formState, formRules);
+
+    watchEffect(() => {
+      if (props.data.uuid) {
+        formState.name = props.data.name;
+        formState.description = props.data.description;
+        formState.defaultEmailLanguage = props.data.defaultEmailLanguage;
+        formState.defaultUserRole = props.data.defaultUserRole;
+      }
+    });
+
+    async function onSubmit () {
+      saving.value = true;
+
+      try {
+        await validate();
+      } catch (error) {
+        saving.value = false;
+        return;
+      }
+
+      try {
+        await store.dispatch('Domain/updateDomain', {
+          ...props.data,
+          ...formState
+        });
+
+        message.success(t('MESSAGES.UPDATE_SUCCESS'));
+      } catch (error) {
+        message.error(error.message || t('ERRORS.COMMON_MESSAGE'));
+      } finally {
+        saving.value = false;
+      }
+    }
+
+    function reset () {
+      formState.name = props.data.name;
+      formState.description = props.data.description;
+      formState.defaultEmailLanguage = props.data.defaultEmailLanguage;
+      formState.defaultUserRole = props.data.defaultUserRole;
+    }
+
+    return {
+      formState,
+      onSubmit,
+      reset,
+      saving,
+      validateInfos,
+      isRootDomain: computed(() => store.getters['Domain/isRootDomain'])
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+ .input-container {
+    label {
+      display: inline-block;
+      font-weight: 600;
+      margin-bottom: 10px;
+    }
+
+    .ant-select {
+      width: 100%;
+    }
+
+    small {
+      font-style: italic;
+      color: @text-color-secondary;
+    }
+  }
+
+  .form-actions {
+    .reset {
+      margin-right: 10px;
+    }
+  }
+</style>
