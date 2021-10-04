c47260b18bd6bb63993ce5b57733d2d3f5faeca2
Dung Trung PHAM <dtpham@linagora.com>
2021-10-04 16:50:55 +0700

    Issue #174: Add base path and remove unecessary dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
