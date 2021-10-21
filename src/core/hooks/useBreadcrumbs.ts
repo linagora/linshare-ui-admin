0ceb72c9eaea8bc4cf7077c3dda05c01f6d35d16
Dung Trung PHAM <dtpham@linagora.com>
2021-10-21 16:08:22 +0700

    Fix breadcrumbs not repspond to route change

--- src/core/hooks/useBreadcrumbs.ts
+++ src/core/hooks/useBreadcrumbs.ts
@@ -1,35 +1,38 @@
-import { useRoute, useRouter } from 'vue-router';
-
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 interface Breadcrumb {
   label: string | unknown;
   path: string;
 }
 
 export default function useBreadcrumbs () {
-  const { meta, path } = useRoute();
-  const { getRoutes } = useRouter();
-  const breadcrumbs: Breadcrumb[] = [{
-    label: meta.label,
-    path
-  }];
+  const { getRoutes, currentRoute } = useRouter();
+  const breadcrumbs = computed<Breadcrumb[]>(() => {
+    const list = [{
+      label: currentRoute.value.meta.label,
+      path: currentRoute.value.path
+    }];
 
-  function addParentRoute (name: string | unknown) {
+    if (currentRoute.value.meta.parentRoute) {
+      addParentRoute(currentRoute.value.meta.parentRoute, list);
+    }
+
+    return list;
+  });
+
+  function addParentRoute (name: string | unknown, list: Breadcrumb[]) {
     const parent = getRoutes().find(route => route.name === name);
 
     if (parent) {
-      breadcrumbs.unshift({
+      list.unshift({
         label: parent.meta.label,
         path: parent.path
       });
 
       if (parent.meta.parentRoute) {
-        addParentRoute(parent.meta.parentRoute);
+        addParentRoute(parent.meta.parentRoute, list);
       }
     }
-  }
-
-  if (meta.parentRoute) {
-    addParentRoute(meta.parentRoute);
   }
 
   return {
