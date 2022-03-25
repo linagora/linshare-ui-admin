24fd7a2a38c147e9f09c78360532125093793912
Dung Trung PHAM <dtpham@linagora.com>
2022-03-25 14:53:48 +0700

    Issue #189: Support using custom label for breadcrumbs

--- src/core/hooks/useBreadcrumbs.ts
+++ src/core/hooks/useBreadcrumbs.ts
@@ -1,7 +1,7 @@
 import { useRouter } from 'vue-router';
 import { computed, ComputedRef } from 'vue';
 interface Breadcrumb {
-  label: string | unknown;
+  label: string;
   path: string;
 }
 
@@ -9,18 +9,28 @@ type UsableBreadcrumbs = {
   breadcrumbs: ComputedRef<Breadcrumb[]>;
 };
 
-export default function useBreadcrumbs(): UsableBreadcrumbs {
+export default function useBreadcrumbs(customs?: Breadcrumb[]): UsableBreadcrumbs {
   const { getRoutes, currentRoute } = useRouter();
   const breadcrumbs = computed<Breadcrumb[]>(() => {
-    const list = [
+    const list: Breadcrumb[] = [
       {
-        label: currentRoute.value.meta.label,
+        label: currentRoute.value.meta.label as string,
         path: currentRoute.value.name as string,
       },
     ];
 
     if (currentRoute.value.meta.parentRoute) {
       addParentRoute(currentRoute.value.meta.parentRoute, list);
+    }
+
+    if (customs) {
+      list.forEach((breadcrumb) => {
+        const toBeUpdate = customs.find((custom) => custom.path === breadcrumb.path);
+
+        if (toBeUpdate) {
+          breadcrumb.label = toBeUpdate.label;
+        }
+      });
     }
 
     return list;
@@ -31,7 +41,7 @@ export default function useBreadcrumbs(): UsableBreadcrumbs {
 
     if (parent) {
       list.unshift({
-        label: parent.meta.label,
+        label: parent.meta.label as string,
         path: parent.name as string,
       });
 
