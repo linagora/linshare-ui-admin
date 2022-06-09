670c9c11f093bb3f376679ab1e520fa6378c2cc1
BUI Van Khoat <bvkhoat@linagora.com>
2022-06-09 11:44:49 +0700

    Issue #250: include pinia package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from '@/core/router';
 import i18n from '@/core/plugins/i18n';
 import store from '@/core/store';
+import { createPinia } from 'pinia'
 import useAntdComponents from '@/core/plugins/antd';
 import { requiresAuthGuard } from './modules/auth/router/requiresAuth.guard';
 import { requiresDomainAccessibility } from './modules/domain/router/requiresDomainAccessibility';
@@ -14,6 +15,6 @@ requiresAuthGuard(router, store);
 requiresDomainAccessibility(router, store);
 requiresFunctionalityToExist(router, store);
 
-const app = createApp(App).use(store).use(router).use(i18n);
+const app = createApp(App).use(store).use(router).use(i18n).use(createPinia());
 
 useAntdComponents(app).mount('#app');
