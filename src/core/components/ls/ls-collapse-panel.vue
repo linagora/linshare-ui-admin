7369107203d8a3d820dcea25045915c21788f128
Khoat Van BUI <bvkhoat@linagora.com>
2022-10-20 15:32:47 +0700

    Issue #337: Wrapper ant collapse

---
+++ src/core/components/ls/ls-collapse-panel.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-collapse-panel class="ls-collapse-panel" v-bind="$attrs">
+    <slot />
+  </a-collapse-panel>
+</template>
+<style lang="less">
+.ls-collapse-panel.ant-collapse-item {
+  border: 1px solid #e4e5f0 !important;
+  border-radius: 16px !important;
+  overflow: hidden;
+
+  .ant-collapse-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 28px !important;
+    background: #ffffff;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 16px !important;
+    color: #434657;
+    border: none !important;
+    overflow: hidden;
+  }
+
+  .ant-collapse-content {
+    border-top: none;
+  }
+
+  .ant-collapse-arrow {
+    width: 44px;
+    height: 44px;
+    background: #ffffff;
+    border: 1px solid #e4e5f0;
+    border-radius: 10px;
+    display: flex !important;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    color: #007aff !important;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/core/components/ls/ls-collapse.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-collapse class="ls-collapse" v-bind="$attrs" expand-icon-position="right">
+    <slot />
+  </a-collapse>
+</template>
+<style lang="less">
+.ls-collapse {
+  border: none !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 24px;
+  background-color: transparent !important;
+}
+</style>
