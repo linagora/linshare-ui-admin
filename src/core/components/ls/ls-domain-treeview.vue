a514bef46a2294fdb9d7409ed02423e9d2b951a3
Khoat Van BUI <bvkhoat@linagora.com>
2022-11-01 23:08:20 +0700

    Issue #331: Implement domain treeview component

---
+++ src/core/components/ls/ls-domain-treeview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="ls-domain-treeview">
+        <div class="ls-domain-treeview__domain-item">
+            <div class="dot"></div>
+            <div class="name">
+                <span>Sub domain - default</span>
+                <span>C</span>
+            </div>
+        </div>
+        <div class="ls-domain-treeview__domain-sub">
+
+        </div>
+    </div>
+
+</template>
+<style lang="less" scoped>
+.ls-domain-treeview {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+}
+</style>
--- src/modules/design-system/pages/form-page.vue
+++ src/modules/design-system/pages/form-page.vue
@@ -5,12 +5,14 @@
       v-model:model-unit="form.unit"
       label="Hello world"
     ></ls-quota-input>
+    <ls-domain-treeview></ls-domain-treeview>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { reactive } from 'vue';
 import LsQuotaInput from '@/core/components/ls/ls-quota-input.vue';
+import LsDomainTreeview from '@/core/components/ls/ls-domain-treeview.vue';
 
 const form = reactive({
   quotaValue: 0,
