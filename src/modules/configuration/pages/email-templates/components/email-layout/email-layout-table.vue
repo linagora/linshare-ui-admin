3829d9fb47c1780dc69f72ec1e5a07f55a88f22a
Khoat Van BUI <bvkhoat@linagora.com>
2023-05-24 15:11:16 +0700

    feat: add default sort order for email layout

--- src/modules/configuration/pages/email-templates/components/email-layout/email-layout-table.vue
+++ src/modules/configuration/pages/email-templates/components/email-layout/email-layout-table.vue
@@ -132,7 +132,8 @@ const columns = computed(() => [
   {
     title: t('GENERAL.MODIFICATION_DATE'),
     key: 'modificationDate',
-    sorter: (a: MailLayout, b: MailLayout) => (a.modificationDate || 0) - (b.modificationDate || 0),
+    sorter: (a: MailLayout, b: MailLayout) => (b.modificationDate || 0) - (a.modificationDate || 0),
+    defaultSortOrder: 'descend',
   },
   {
     title: t('EMAIL_TEMPLATES.VISIBILITY'),
