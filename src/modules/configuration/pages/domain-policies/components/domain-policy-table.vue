b6328e165d314b9df032178db61b80baca4b8caa
Khoat Van BUI <bvkhoat@linagora.com>
2023-10-07 11:09:07 +0700

    fix: resolve height limit on domain policy table dropdown actions

--- src/modules/configuration/pages/domain-policies/components/domain-policy-table.vue
+++ src/modules/configuration/pages/domain-policies/components/domain-policy-table.vue
@@ -250,7 +250,8 @@ function allowAssigned() {
       padding: 8px;
       gap: 8px;
       width: 301px;
-      height: 164px;
+      min-height: 164px;
+      height: fit-content;
       background: #ffffff;
       box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.08), 0px 8px 24px rgba(0, 0, 0, 0.08);
       border-radius: 8px;
