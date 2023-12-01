0fb4a0e96ab2560de80b1590f2a1a14d10e56b1a
Khoat Van BUI <bvkhoat@linagora.com>
2023-12-01 10:29:00 +0700

    feat: add search function upgrade task

--- src/modules/upgrades/components/upgrade-list-actions.vue
+++ src/modules/upgrades/components/upgrade-list-actions.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="upgrade-list-actions">
+    <a-input
+      v-model:value="filterText"
+      :placeholder="$t('UPGRADE_TASK.SEARCH')"
+      class="upgrade-list-actions__action-input"
+    >
+      <template #prefix>
+        <SearchOutlined />
+      </template>
+    </a-input>
     <a-button class="ls-button ls-refresh" @click="emits('refresh')">
       <a-spin v-if="loading" />
       <div v-else>
@@ -10,11 +19,13 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ReloadOutlined } from '@ant-design/icons-vue';
+import { ReloadOutlined, SearchOutlined } from '@ant-design/icons-vue';
+import useUpgradeTasks from '../hooks/useUpgradeTasks';
 
 defineProps<{
   loading: boolean;
 }>();
+const { filterText } = useUpgradeTasks();
 // props & emits
 const emits = defineEmits(['refresh']);
 </script>
@@ -22,12 +33,24 @@ const emits = defineEmits(['refresh']);
 .upgrade-list-actions {
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
   gap: 12px;
 
+  &__action-input {
+    background: #fafafa;
+    border: 1px solid #e4e5f0;
+    border-radius: 10px;
+    height: 44px;
+    width: 400px;
+    input {
+      background-color: transparent;
+    }
+  }
+
   .ls-refresh {
     min-width: 130px;
+    color: @primary-color;
   }
 }
 </style>
